e6af5628ad4d0b5ccc60bc746291de94868aaf18
Đặng Thanh Tuấn <contact.dangthanhtuan@gmail.com>
2019-06-24 11:01:55 +0700

    fix layout

--- QuanLyNhaHang/QuanLyNhaHang/Model/API.cs
+++ QuanLyNhaHang/QuanLyNhaHang/Model/API.cs
@@ -264,6 +264,7 @@ namespace QuanLyNhaHang.Model
                             "\", \"status\": \"" + table.status +
                             "\", \"customer\": { \"fullName\": \"" + table.customer.fullName +
                             "\", \"phone\": \"" + table.customer.phone + "\"}}";
+
             string url1 = SERVER + "/bills";
             string json1 = "{\"employeeID\": \"" + employeeID +
                             "\", \"number\": \"" + table.number +
